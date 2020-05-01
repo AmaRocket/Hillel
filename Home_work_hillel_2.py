611f0cdc09665f0a5ee91d199356ef5ec7b4a274
AmaRocket <63898437+AmaRocket@users.noreply.github.com>
2020-05-01 19:57:54 +0300

    загрузка дз в репозитарий

---
+++ Home_work_hillel_2.py
@@ -0,0 +1,65 @@
+"""
+import antigravity
+	import __hello__
+	from __future__ import braces
+
+
+
+
+"""
+#from __future__ import braces - очень смешно)
+# import antigravity - комикс прочитал
+# import __hello__
+import this #(нашел пока гуглил)
+
+'''
+Вывести на консоль своё имя, нарисованное 'звёздочками'. Для выполнения этого задания надо использовать не только 
+символы звёздочки и пробела, но и ESCAPE символы: \n и \t
+
+*       *         *         *       * *********
+**      *        * *        **     ** *
+* *     *       *   *       * *   * * *
+*  *    *      *     *      *  * *  * *******
+*   *   *     *       *     *   *   * *
+*    *  *    ***********    *       * *
+*     * *   *           *   *       * *
+*      **  *             *  *       * *
+*       * *               * *       * **********
+
+'''
+
+print(
+    "***\t\t\t*  \t\t\t*\t\t  *\t\t\t*\t\t\t*********\t\t  ********\t\t********\t *\t\t  *\t  ***********\t***********  ***\n"
+    "*\t\t   * *\t\t\t**\t\t **\t\t   * *\t\t\t*\t\t  *\t\t*\t\t   *   *\t\t*\t *\t\t  *\t  *\t\t\t*\t*\t *\t  *\t   *\n"
+    "*\t\t  *   *\t\t\t* * \t* *\t\t  *   *\t\t\t*\t\t  *\t\t*\t\t   *   *\t\t\t *\t\t  *\t  *\t\t\t\t\t *\t\t   *\n"
+    "*\t\t *\t   *\t\t*  *   *  *\t\t *\t   *\t\t*\t\t  *\t\t*\t\t   *   *\t\t\t *\t\t  *   *\t\t\t\t\t *\t\t   *\n"
+    "*\t\t* \t\t*\t\t*   * *   *\t\t*\t\t*\t\t*\t\t  *\t\t*\t\t   *   *\t\t\t *\t\t *\t  *\t\t*\t\t\t *\t\t   *\n"
+    "*\t   ***********\t\t*\t *\t  *\t   **********\t\t*********\t\t*\t\t   *   *\t\t\t ********\t  *******\t\t\t *\t\t   *\n"
+    "*\t  *\t\t\t  *\t\t*\t\t  *\t  *\t\t\t  *\t\t*\t\t*\t\t*\t\t   *   *\t\t\t *\t\t *\t  *\t    *\t\t\t *\t\t   *\n"
+    "*\t *\t\t\t   *\t*\t\t  *\t *\t\t\t   *\t*\t\t *\t\t*\t\t   *   *\t\t*\t *\t\t  *\t  *\t\t    *\t\t *\t\t   *\n"
+    "*** *\t\t\t\t*\t*\t\t  *\t*\t\t\t\t*\t*\t\t  *\t\t  ********\t\t********\t *\t\t  *\t  ***********\t\t***\t\t ***")
+
+'''
+Написать программу, которая выводит в консоль таблицу Escape-последовательностей:
+
+Escape sequences
+\a      Bell (alert)
+\b      Backspace
+\n      New line
+\t      Horizontal tab
+\\      Backslash \
+\"      Double quotation mark "
+\'      Single quotation mark '
+
+
+'''
+print(
+    '\n'
+    'Escape sequences\n'
+    '\\a      Bell (alert) \n'
+    '\\b      Backspace \n'
+    '\\n      New line \n'
+    '\\t      Horizontal tab \n'
+    '\\       Backslash \ \n'
+    '\\"      Double quotation mark \n'
+    '\\\'      Single quotation mark  \n')
